205b5dc116ddfd753721c312f653473fd4362189
Maximus <maks.shherbakov04@gmail.com>
2024-06-02 21:47:09 +0200

    initialized special pointer type

--- Pointers/pointers.go
+++ Pointers/pointers.go
@@ -7,6 +7,9 @@ import (
 func main() {
 	age := 19 // regular variable
 
+	// var agePointer *int  // the 2nd method how to initialize
+	agePointer := &age
+
 	fmt.Println("Age: ", age)
 
 	otherYears := getOtherYears(age)
