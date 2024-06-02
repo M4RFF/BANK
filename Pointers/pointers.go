6d898790e5dc4dd6cfe034695e74d63fd544af32
Maximus <maks.shherbakov04@gmail.com>
2024-06-02 22:07:22 +0200

    Using Pointers & Passing Pointers To Functions

--- Pointers/pointers.go
+++ Pointers/pointers.go
@@ -12,13 +12,14 @@ func main() {
 
 	// agePointer := &age // the 2nd method how to initialize
 
-	// fmt.Println("Age:", agePointer)  // it will output the address 0xc00000a0b8
-	fmt.Println("Age:", *agePointer) // it will output the value
+	// fmt.Println("Age:", agePointer)  // it will output the address "0xc00000a0b8"
+	fmt.Println("Age:", *agePointer) // it will output the value "19"
 
-	// otherYears := getOtherYears(age)
-	// fmt.Println(otherYears)
+	// Now there's no copies of 19 its only one single "19" value
+	otherYears := getOtherYears(agePointer)
+	fmt.Println(otherYears)
 }
 
-func getOtherYears(age int) int {
-	return age - 14
+func getOtherYears(age *int) int {
+	return *age - 14
 }
