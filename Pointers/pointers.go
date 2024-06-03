2d7815384c7d868aed1e703ed4218b67e4558311
Maximus <maks.shherbakov04@gmail.com>
2024-06-03 19:47:00 +0200

    Using Pointers for Data Mutation

--- Pointers/pointers.go
+++ Pointers/pointers.go
@@ -8,6 +8,7 @@ func main() {
 	age := 19 // regular variable
 
 	var agePointer *int
+
 	agePointer = &age
 
 	// agePointer := &age // the 2nd method how to initialize
@@ -16,10 +17,14 @@ func main() {
 	fmt.Println("Age:", *agePointer) // it will output the value "19"
 
 	// Now there's no copies of 19 its only one single "19" value
-	otherYears := getOtherYears(agePointer)
-	fmt.Println(otherYears)
+	// otherYears := getOtherYears(agePointer)
+	// fmt.Println(otherYears)
+
+	edutAgeToAdultYears(agePointer)
+	fmt.Println(age)
 }
 
-func getOtherYears(age *int) int {
-	return *age - 14
+func edutAgeToAdultYears(age *int) {
+	// return *age - 14
+	*age = *age - 14 // we stored it in our pointer
 }
