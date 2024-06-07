975c5d505f863a668a221333e63d614bd3626000
Maximus <maks.shherbakov04@gmail.com>
2024-06-07 16:21:55 +0200

    Working with Type Switches

--- Interfaces & Generic Code/main.go
+++ Interfaces & Generic Code/main.go
@@ -48,6 +48,8 @@ func main() {
 		return
 	}
 
+	printSomething(todo)
+
 	userNote, err := note.New(title, content)
 
 	if err != nil {
@@ -71,7 +73,14 @@ func main() {
 
 // Special Kinder of Interface
 func printSomething(value interface{}) { // any values are allowed
-	fmt.Println(value)
+	switch value.(type) {
+	case int:
+		fmt.Println("Integer:", value)
+	case float64:
+		fmt.Println("Float:", value)
+	case string:
+		fmt.Println(value)
+	}
 }
 
 func outputData(data Outputtable) error {
