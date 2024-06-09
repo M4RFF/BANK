287b1bd66ae9458354e40187d19bac566879c294
Maximus <maks.shherbakov04@gmail.com>
2024-06-09 17:30:49 +0200

    Building Dynamic Lists With Slices

--- ManagingRD/lists.go
+++ ManagingRD/lists.go
@@ -3,26 +3,40 @@ package main
 import "fmt"
 
 func main() {
-	var productNames [4]string = [4]string{"A Book"}
-	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+	prices := []float64{10.99, 8.99}
+	fmt.Println(prices)
+	prices[1] = 13.13
+
+	// adding a new element to the slice
+	prices = append(prices, 13.14)
 	fmt.Println(prices)
 
-	productNames[2] = "A Carpet"
-
-	fmt.Println(productNames)
-
-	fmt.Println(prices[2])
-
-	featuredPrices := prices[1:]
-	featuredPrices[0] = 199.9
-	highlightedPrices := featuredPrices[:1]
-	// fmt.Println(featuredPrices)
-	// fmt.Println(prices)
-	// fmt.Println(highlightedPrices)
-	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
-
-	highlightedPrices = highlightedPrices[:3]
-	fmt.Println(highlightedPrices)
-	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
-
+	// removing an element from the slice
+	prices = prices[1:]
+	fmt.Println(prices)
 }
+
+// func main() {
+// 	var productNames [4]string = [4]string{"A Book"}
+// 	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+// 	fmt.Println(prices)
+
+// 	productNames[2] = "A Carpet"
+
+// 	fmt.Println(productNames)
+
+// 	fmt.Println(prices[2])
+
+// 	featuredPrices := prices[1:]
+// 	featuredPrices[0] = 199.9
+// 	highlightedPrices := featuredPrices[:1]
+// 	// fmt.Println(featuredPrices)
+// 	// fmt.Println(prices)
+// 	// fmt.Println(highlightedPrices)
+// 	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
+
+// 	highlightedPrices = highlightedPrices[:3]
+// 	fmt.Println(highlightedPrices)
+// 	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
+
+// }
