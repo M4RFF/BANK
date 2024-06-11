20c33b59adccb7587d2945ad92383c7881d0fc50
Maximus <maks.shherbakov04@gmail.com>
2024-06-11 21:54:33 +0200

    Adding a Method

--- PriceCalculator/main.go
+++ PriceCalculator/main.go
@@ -1,20 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"price-calculator/prices"
+)
 
 func main() {
-	prices := []float64{10, 20, 30}
+	// prices := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.3, 0.1, 0.13}
 
-	result := make(map[float64][]float64)
+	// result := make(map[float64][]float64)
 
 	for _, taxRate := range taxRates {
-		taxIncludePrice := make([]float64, len(prices))
-		for priceIndex, price := range prices {
-			taxIncludePrice[priceIndex] = price * (1 + taxRate)
-		}
-		result[taxRate] = taxIncludePrice
+		priceJob := prices.NewTaxtIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
 
-	fmt.Println(result)
 }
