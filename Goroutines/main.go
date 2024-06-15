e207071143fb8b5e41044192fcfd125d752d1dd5
Maximus <maks.shherbakov04@gmail.com>
2024-06-15 11:58:02 +0200

    Introducing & Using Channels

--- Goroutines/main.go
+++ Goroutines/main.go
@@ -9,14 +9,20 @@ func greet(phrase string) {
 	fmt.Println("Hello!", phrase)
 }
 
-func slowGreet(phrase string) {
+func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long talking task
 	fmt.Println("Hello!", phrase)
+	doneChan <- true
 }
 
 func main() {
-	go greet("It's my first golang course!")
-	go greet("How is it going?")
-	go slowGreet("How ... is ... it ... going ...?")
-	go greet("I hope you're doing great")
+	// go greet("It's my first golang course!")
+	// go greet("How is it going?")
+
+	// creating a channel below:
+	done := make(chan bool)
+	go slowGreet("How ... is ... it ... going ...?", done)
+	// fmt.Println(<-done)
+	<-done
+	// go greet("I hope you're doing great")
 }
