3f79805cf0d1c42b19cb5fe640ec07a29c6b6e53
Maximus <maks.shherbakov04@gmail.com>
2024-06-16 19:04:01 +0200

    Registering a POST Route

--- REST-API/main.go
+++ REST-API/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"rest-api/models"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,10 +11,30 @@ func main() {
 	server := gin.Default() // creates a server
 
 	server.GET("/events", getEvents) // GET, POST, PUT, PATCH, DELETE
+	server.POST("/events", postEvent)
 
 	server.Run(":1313") // localhost 1313
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func postEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event) // kind of similar with fmt.Scan(&...)
+
+	if err != nil {
+		// respond to client that smth went wrong (400).
+		// gin.H() creates a map that display a message
+		context.JSON(http.StatusBadRequest, gin.H{"message": "couldn't parse  request data"})
+		// if it went wrong we need to return
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	// send a respond if everything works
+	context.JSON(http.StatusCreated, gin.H{"message": "event created!", "event": event})
 }
--- REST-API/models/event.go
+++ REST-API/models/event.go
@@ -4,16 +4,20 @@ import "time"
 
 type Event struct {
 	ID          int
-	Name        string
-	Description string
-	Location    string
-	DateTime    time.Time
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
 	UserID      int
 }
 
 var events = []Event{}
 
-func (evt Event) Save() {
+func (evt Event) Save() { // save an event
 	// later: add it to a database
 	events = append(events, evt) // added a new event (evt)
+}
+
+func GetAllEvents() []Event { // not for existing events, it is for availible events
+	return events
 }
