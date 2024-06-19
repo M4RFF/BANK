715cd2998679e32cc3785b65f4d0e8b03a053374
Maximus <maks.shherbakov04@gmail.com>
2024-06-19 22:58:45 +0200

    Soring Data in tge DataBase (INSERT)

--- REST-API/models/event.go
+++ REST-API/models/event.go
@@ -1,9 +1,12 @@
 package models
 
-import "time"
+import (
+	"rest-api/db"
+	"time"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -13,11 +16,32 @@ type Event struct {
 
 var events = []Event{}
 
-func (evt Event) Save() { // save an event
+func (evt Event) Save() error { // save an event
+	query := `
+	INSERT INTO events(name, description, location, dateTime, user_id) 
+	VALUES (?, ?, ?, ?, ?)`
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	result, err := stmt.Exec(evt.Name, evt.Description, evt.Location, evt.DateTime, evt.UserID)
+
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	evt.ID = id
+	return err
 	// later: add it to a database
-	events = append(events, evt) // added a new event (evt)
+	// events = append(events, evt) // added a new event (evt)
 }
 
 func GetAllEvents() []Event { // not for existing events, it is for availible events
+
 	return events
 }
