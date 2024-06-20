513bd3a71e2c47e52c41bc8f4bef5dce048bf082
Maximus <maks.shherbakov04@gmail.com>
2024-06-20 13:39:28 +0200

    Created the 2nd The REST API

---
+++ TheREST-API/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// configers an http server
+	server := gin.Default()
+
+	server.GET("/events", getEvents) // GET, POST, DELETE, PUT, PATCH
+
+	// start it and starts listening for incomming requests
+	server.Run(":1331") // loacalhost: 1331
+}
+
+func getEvents(context *gin.Context) {
+	// send back a respond by JSON function
+	context.JSON(http.StatusOK, gin.H{"message": "Hello!"}) // stores tthe number 200
+}
