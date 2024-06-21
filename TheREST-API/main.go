0cba0a3436a38dafb1a1cef42fae273cdc19b033
Maximus <maks.shherbakov04@gmail.com>
2024-06-21 15:45:16 +0200

    Registrating a POST Rate

--- TheREST-API/main.go
+++ TheREST-API/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"theRestApi/models"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,12 +13,33 @@ func main() {
 
 	server.GET("/events", getEvents) // GET, POST, DELETE, PUT, PATCH
 
+	server.POST("/events", postEvent) // the same path /events
+
 	// start it and starts listening for incomming requests
 	server.Run(":1331") // loacalhost: 1331
 }
 
 func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
 	// send back a respond by JSON function
+	context.JSON(http.StatusOK, events) // stores the number 200
+}
 
-	context.JSON(http.StatusOK, gin.H{"message": "Hello!"}) // stores tthe number 200
+func postEvent(context *gin.Context) { // we use again (context *gin.Context)
+	// we need to get some data of incoming requests
+	var event models.Event
+	// if some filds are missing it's not a problem, cus'line bellow
+	// just set a missing filed as zero
+	err := context.ShouldBindJSON(&event) // a little bit works like a Scan()
+
+	if err != nil {
+		// it send back a respond that tells a user that smth went wrong
+		context.JSON(http.StatusBadRequest, gin.H{"message": "couldn't parse request data"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	context.JSON(http.StatusCreated, gin.H{"message": "event created", "event": event})
+
 }
--- TheREST-API/models/event.go
+++ TheREST-API/models/event.go
@@ -5,10 +5,10 @@ import "time"
 // created a custom type that shape
 type Event struct {
 	ID          int
-	Name        string
-	Description string
-	Location    string
-	DateTime    time.Time
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
 	UserID      int
 }
 
@@ -22,4 +22,9 @@ func (evt Event) Save() {
 
 	// appending a new event to events
 	events = append(events, evt)
+}
+
+// get all avaliable events
+func GetAllEvents() []Event {
+	return events
 }
