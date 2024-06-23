f4a02f2ed756839f08336988ea59c3f0d41316a5
Maximus <maks.shherbakov04@gmail.com>
2024-06-23 12:36:53 +0200

    Adding A Sql Database + comments

--- TheREST-API/db/db.go
+++ TheREST-API/db/db.go
@@ -6,15 +6,16 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-var DB *sql.DB
+var DB *sql.DB // a global variable
 
+// initializinga data base
 func InitDB() {
 	DB, err := sql.Open("sqlite3", "api.db") // open the connection
 
 	if err != nil {
-		panic("couldn't connect to database")
+		panic("couldn't connect to database") // crash the app
 	}
 
-	DB.SetMaxOpenConns(10) // we have a pool of outgoing connections
+	DB.SetMaxOpenConns(10) // how many connections can be open
 	DB.SetMaxIdleConns(5)  // how many connections we wanna keep open if nobody is using them
 }
