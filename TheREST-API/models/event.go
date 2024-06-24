807da1bc1af826b1b6cbc1fe477719bda125b393
Maximus <maks.shherbakov04@gmail.com>
2024-06-24 17:40:38 +0200

    Sorting Data in the DataBase (ISERT)

--- TheREST-API/models/event.go
+++ TheREST-API/models/event.go
@@ -1,10 +1,13 @@
 package models
 
-import "time"
+import (
+	"theRestApi/db"
+	"time"
+)
 
 // created a custom type that shape
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -17,11 +20,28 @@ type Event struct {
 
 var events = []Event{}
 
-func (evt Event) Save() {
+func (evt Event) Save() error {
 	// LATER: add it to a database
+	query := `
+	INSERT INTO events(name, secription, location, dateTime, user_id) 
+	VALUES (?, ?, ?, ?, ?)`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	result, err := stmt.Exec(evt.Name, evt.Description, evt.Location, evt.DateTime, evt.UserID)
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	evt.ID = id
+	return err
 
 	// appending a new event to events
-	events = append(events, evt)
+	// events = append(events, evt)
 }
 
 // get all avaliable events
